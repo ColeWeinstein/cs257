7037d7fb50350a12ad2f18703fb85b1e3137c7df
Thien K. M. Bui <buik@carleton.edu>
2021-09-19 10:47:37 -0500

    added comments

--- gitintro/hello.py
+++ gitintro/hello.py
@@ -1,6 +1,13 @@
+Authors: Kevin Bui and Cole Weinstein 
+This file contains code for the git example lab
+
+
 def main():
+    
     print("Hello World")
+    # Bui's revision
     print("testing this and see if it work")
+    #Weinstein's revision (ran into merge conflict because same line at 1 point)
     print("you've been hacked lol")
 
 if __name__ == "__main__":
